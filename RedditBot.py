fb08255513d3dace79c04438f23e83ec682ebaf7
Nour125 <ahmed.noureddin.00@gmail.com>
2023-02-26 19:37:50 +0100

    voice verbesert

--- RedditBot.py
+++ RedditBot.py
@@ -1,8 +1,10 @@
 import datetime
 import pyttsx3
-import gtts
 import random
+import gtts
 from playsound import playsound
+from gtts.tokenizer import pre_processors
+import moviepy.editor as video_editor
 import praw
 from praw.models import MoreComments
 engine = pyttsx3.init()
@@ -28,14 +30,14 @@ def hotsub():
     reddit.read_only = True
     subfound = False
     while(subfound == False):
-
+        #git Submissios
         for submission in subReddit.hot(limit = 100):
             submissionList.append(submission) 
         
         x = random.randrange(100)
         sub = submissionList[x].title
         cursub = submissionList[x]
-        
+        #git Comment
         cursub.comments.replace_more(limit = 0)
         #submission.comment_sort = "new"  #Macht iwie nichts    
         commentsList = list(cursub.comments) 
@@ -43,17 +45,12 @@ def hotsub():
         topcom = commentsList[0]
         com = topcom.body
         commentwords = com.split(" ")
-        #firstlen = commentwords
         #was wenn nichts gefunden wurde
         while((len(commentwords) < 75) & ((i+1) != len(commentsList))):
             topcom = commentsList[i]
-            print(len(commentsList))
-            print(i)
             i += 1
             com = topcom.body
             commentwords = com.split(" ")
-            #curlen = len(commentwords)
-
         if ((i+1) == len(commentsList)):
             subfound = False
         else:
@@ -63,36 +60,21 @@ def hotsub():
     return (sub +"\n"+ com)
 
 
-def topsub():
-    reddit.read_only = True
-
-    for submission in subReddit.top(limit = 100):
-        submissionList.append(submission) 
-    
-    sub = submissionList[0].title
-    cursub = submissionList[0]
-    
-    cursub.comments.replace_more(limit = 0)
-    #submission.comment_sort = "new"      
-    commentsList = list(cursub.comments) 
-    i = 1
-    topcom = commentsList[0]
-    com = topcom.body
-    commentwords = com.split(" ")
-    while(len(commentwords) < 50):
-        topcom = commentsList[i]
-        i += 1
-        com = topcom.body
-        commentwords = com.split(" ")
-    reddit.read_only = False
-
-    return (sub +" "+ com)
-
+def pre_processing(text):
+    pre_processors.tone_marks(text)
+    pre_processors.end_of_line(text)
+    pre_processors.abbreviations(text)
+    pre_processors.word_sub(text)
 
 def tts (text):
-    x = gtts.gTTS(text)
+    x = gtts.gTTS(text, lang ='en', tld ='us')
     x.save(zus)
    
+
+
+
+
 y = hotsub()
+pre_processing(y)
 tts(y)
 print("done")
