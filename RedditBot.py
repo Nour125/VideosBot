b1179c975a12d7822e558b547ad8ae19e03d58ef
Nour125 <ahmed.noureddin.00@gmail.com>
2023-02-26 12:47:58 +0100

    Random sub done, correct length of comment done

--- RedditBot.py
+++ RedditBot.py
@@ -1,6 +1,7 @@
 import datetime
 import pyttsx3
 import gtts
+import random
 from playsound import playsound
 import praw
 from praw.models import MoreComments
@@ -9,7 +10,10 @@ date = datetime.datetime.now()
 mp3 = ".mp3"
 zus = date.strftime("%d.%m.%Y") + mp3
 commentsList = []
+submissionList = []
+commentwords = list()
 
+lenOfCom = 0
 reddit = praw.Reddit(
     password="123poi??",
     username="ReadB629",
@@ -22,34 +26,66 @@ reddit = praw.Reddit(
 subReddit = reddit.subreddit("askReddit")
 def hotsub():
     reddit.read_only = True
-    for submission in subReddit.hot(limit = 1):
-        cursub = submission
-        sub = submission.title
+    subfound = False
+    while(subfound == False):
 
-    submission.comment_sort = "Top"
-    submission.comments.replace_more(limit=1)
-    for comment in cursub.comments:
-        if isinstance(comment, MoreComments):
-            continue
-        com = comment.body
-        #print(comment.body)
+        for submission in subReddit.hot(limit = 100):
+            submissionList.append(submission) 
+        
+        x = random.randrange(100)
+        sub = submissionList[x].title
+        cursub = submissionList[x]
+        
+        cursub.comments.replace_more(limit = 0)
+        #submission.comment_sort = "new"  #Macht iwie nichts    
+        commentsList = list(cursub.comments) 
+        i = 0
+        topcom = commentsList[0]
+        com = topcom.body
+        commentwords = com.split(" ")
+        #firstlen = commentwords
+        #was wenn nichts gefunden wurde
+        while((len(commentwords) < 75) & ((i+1) != len(commentsList))):
+            topcom = commentsList[i]
+            print(len(commentsList))
+            print(i)
+            i += 1
+            com = topcom.body
+            commentwords = com.split(" ")
+            #curlen = len(commentwords)
+
+        if ((i+1) == len(commentsList)):
+            subfound = False
+        else:
+            subfound = True
+    
     reddit.read_only = False
-    return (sub +"  "+ com)
+    return (sub +"\n"+ com)
 
 
 def topsub():
     reddit.read_only = True
-    for submission in subReddit.top(limit = 1):
-        cursub = submission
-        sub = submission.title
 
-    submission.comments.replace_more(limit = 0)
-    #submission.comment_sort = "new"
-    commentsList = list(submission.comments) 
+    for submission in subReddit.top(limit = 100):
+        submissionList.append(submission) 
+    
+    sub = submissionList[0].title
+    cursub = submissionList[0]
+    
+    cursub.comments.replace_more(limit = 0)
+    #submission.comment_sort = "new"      
+    commentsList = list(cursub.comments) 
+    i = 1
     topcom = commentsList[0]
     com = topcom.body
+    commentwords = com.split(" ")
+    while(len(commentwords) < 50):
+        topcom = commentsList[i]
+        i += 1
+        com = topcom.body
+        commentwords = com.split(" ")
     reddit.read_only = False
-    
+
     return (sub +" "+ com)
 
 
@@ -57,6 +93,6 @@ def tts (text):
     x = gtts.gTTS(text)
     x.save(zus)
    
-y = topsub()
-print(y)
+y = hotsub()
+tts(y)
 print("done")
