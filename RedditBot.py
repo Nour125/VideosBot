900f147f2cb7be6a577f0d23a4c1bc2ac8333817
Nour125 <ahmed.noureddin.00@gmail.com>
2023-03-04 10:08:59 +0100

    Code von gerstern debugt

--- RedditBot.py
+++ RedditBot.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+import math
 import pyttsx3
 import random
 import gtts
@@ -10,7 +12,9 @@ from praw.models import MoreComments
 engine = pyttsx3.init()
 date = datetime.datetime.now()
 mp3 = ".mp3"
-zus = date.strftime("%d.%m.%Y") + mp3
+mp4 = ".mp4"
+zus_audio_name = date.strftime("%d.%m.%Y") + mp3
+zus_video_name = date.strftime("%d.%m.%Y") + mp4
 commentsList = []
 submissionList = []
 commentwords = list()
@@ -68,8 +72,20 @@ def pre_processing(text):
 
 def tts (text):
     x = gtts.gTTS(text, lang ='en', tld ='us')
-    x.save(zus)
+    x.save(zus_audio_name)
+
+def get_audio():
+    #hier musst du die sub aufrufen damit du den name bekommst
+    return video_editor.AudioFileClip("01.03.2023.mp3")
    
+def make_suitable_background_video(video):
+    audio = get_audio()
+    start = random.randrange(1, 1800)
+    #Bestimmt wie viel Sekunden ausm video genommen werden
+    ende  = start + math.ceil(audio.duration) 
+    cur = video_editor.VideoFileClip(video).subclip(start, ende)
+    cur.write_videofile(zus_video_name)
+    
 
 def cut_one_min(video):
     start = random.randrange(1, 180)
@@ -87,16 +103,20 @@ def add_ten_sekunden(video):
     final_clip = video_editor.concatenate_videoclips([video,ten_sec_clip])
     return final_clip
 
-def create_video(video, subaudio):
-    audio = video_editor.AudioFileClip(subaudio)
+
+# can you pass a video?? das kann das problem sein vllt mach alles in eine methode 
+# wenn das nicht geht: mach einfach "die add_ten_sekunden" weg und stat "cut_one_min" mach eine mehtode die
+# dirkt nach die länge des audios ein video erstelt und speichert mit dem datum als name 
+def create_final_video(video):
+    #audio = video_editor.AudioFileClip(subaudio)
+    audio = get_audio()
     clip = video_editor.VideoFileClip(video)
-    while(clip.duration < audio.duration):
-        clip = add_ten_sekunden(clip)
-    clip = video_editor.VideoFileClip.subclip(0,audio.duration)
     clip = clip.volumex(0.0)
-    #finalaudio = video_editor.CompositeAudioClip([clip.audio, audio])
     clip = clip.set_audio(audio)
-    return clip
+    clip.write_videofile("final.mp4")
+    #lösch den background_video
+    os.remove(zus_video_name)
+
 
 
 
@@ -105,9 +125,6 @@ def create_video(video, subaudio):
 #pre_processing(y)
 #print(y)
 #tts(y)
-f = cut_one_min("m.mp4")
-d = create_video(f ,"01.03.2023.mp3")
-d.write_videofile("final.mp4")
-#test = cut_ten_Sekunden("m.mp4")
-#test.write_videofile("final.mp4")
+make_suitable_background_video("m.mp4")
+create_final_video(zus_video_name)
 print("done")
