0bfa912f4e1896171df1dbda403915af2ac46eab
Nour125 <ahmed.noureddin.00@gmail.com>
2023-03-05 13:11:37 +0100

    video erstellung fertig

--- RedditBot.py
+++ RedditBot.py
@@ -20,7 +20,8 @@ final_video_name = date.strftime("%d.%m.%Y") + ".final" + mp4
 commentsList = []
 submissionList = []
 commentwords = list()
-thirty_minute_videos = ["minecraft"]
+thirty_minute_videos = ["minecraft.mp4","Rocket_League.mp4"]
+
 
 lenOfCom = 0
 reddit = praw.Reddit(
@@ -33,20 +34,27 @@ reddit = praw.Reddit(
 
 
 subReddit = reddit.subreddit("askReddit")
+
 def hotsub():
     reddit.read_only = True
     subfound = False
     while(subfound == False):
         #git Submissios
-        for submission in subReddit.hot(limit = 100):
+        for submission in subReddit.hot(limit = 10):
             submissionList.append(submission) 
         
-        x = random.randrange(100)
-        sub = submissionList[x].title
-        cursub = submissionList[x]
+        x = random.randrange(10)
+        if(submissionList[x].over_18 == True):
+            sub = "NSFW" + submissionList[x].title
+        else:
+            sub = submissionList[x].title
+        cursub = submissionList[x] # du brauchst es für die screanshot vom title wie weiss ich noch nicht :(
+        global SubTest
+        SubTest = x
+
         #git Comment
         cursub.comments.replace_more(limit = 0)
-        #submission.comment_sort = "new"  #Macht iwie nichts    
+        #submission.comment_sort = "new"  #Macht iwie nichts :(
         commentsList = list(cursub.comments) 
         i = 0
         topcom = commentsList[0]
@@ -58,12 +66,13 @@ def hotsub():
             i += 1
             com = topcom.body
             commentwords = com.split(" ")
+
         if ((i+1) == len(commentsList)):
             subfound = False
         else:
             subfound = True
-    
-    reddit.read_only = False
+
+    reddit.read_only = False  
     return (sub +"\n"+ com)
 
 
@@ -80,18 +89,20 @@ def tts (text):
 def get_audio():
     #hier musst du die sub aufrufen damit du den name bekommst
     return video_editor.AudioFileClip("01.03.2023.mp3")
-def get_thirty_minute_videos():
-    r = random.randrange( 0 , len(thirty_minute_videos) - 1)
+
+def get_thirty_minute_video():
+    r = random.randrange( 0 , len(thirty_minute_videos) )
     return thirty_minute_videos[r]
    
 def make_suitable_background_video(video):
-    audio = get_audio()
+    audio = video_editor.AudioFileClip(zus_audio_name)
     start = random.randrange(1, 1800)
     #Bestimmt wie viel Sekunden ausm video genommen werden
     ende  = start + math.ceil(audio.duration) 
     cur = video_editor.VideoFileClip(video).subclip(start, ende)
     cur.write_videofile(zus_video_name)
-    
+    cur.reader.close()
+    cur.close()    
 
 def cut_one_min(video):
     start = random.randrange(1, 180)
@@ -101,7 +112,6 @@ def cut_one_min(video):
     cur = video_editor.VideoFileClip(video).subclip(start * 10, ende * 10)
     return cur
 
-
 def add_ten_sekunden(video):
     start = random.randrange(1, 180)
     ende  = start + 1 #Bestimmt wie viel Sekunden ausm video genommen werden
@@ -109,25 +119,24 @@ def add_ten_sekunden(video):
     final_clip = video_editor.concatenate_videoclips([video,ten_sec_clip])
     return final_clip
 
-
-# can you pass a video?? das kann das problem sein vllt mach alles in eine methode 
-# wenn das nicht geht: mach einfach "die add_ten_sekunden" weg und stat "cut_one_min" mach eine mehtode die
-# dirkt nach die länge des audios ein video erstelt und speichert mit dem datum als name 
+# can you pass a video?? das kann das problem sein vllt mach alles in eine methode!
 def create_final_video(video, subaudio):
     audio = video_editor.AudioFileClip(subaudio)
     clip = video_editor.VideoFileClip(video)
     clip = clip.volumex(0.0)
     clip = clip.set_audio(audio)
     clip.write_videofile(final_video_name)
+    clip.reader.close()
+    clip.close()
     #lösch den background_video muss noch gemacht werden
     
-#funktioniert nicht obwohl ich den schliße kommt dies fehler [WinError 32] Der Prozess
+#funktioniert nicht obwohl ich den schliße. Es kommt diese fehler [WinError 32] Der Prozess
 #kann nicht auf die Datei zugreifen, da sie von einem anderen Prozess verwendet wird
-def delete_background_video(name):
-    path = os.path.abspath(name)
-    print(path)
-    video_editor.VideoClip.close(name)
-    os.remove(path)
+#def delete_background_video(name):
+    #path = os.path.abspath(name)
+    #print(path)
+    #video_editor.VideoClip.close(name)
+    #os.remove(path)
 
 #mach die main methode
 
@@ -138,7 +147,7 @@ def main():
         day = datetime.datetime.now
         day = day + datetime.timedelta(days = 1)
         while(datetime.date.day() == day ):
-            #einfach warten bis der nächte tag kommt dann  nochmla was posten
+            #einfach warten bis der nächte tag kommt dann nochmla was posten
             x = x
 
 
@@ -146,6 +155,7 @@ y = hotsub()
 pre_processing(y)
 print(y)
 tts(y)
-make_suitable_background_video(get_thirty_minute_videos())
+print("hier ist die subtest " + submissionList[SubTest].url)
+make_suitable_background_video(get_thirty_minute_video())
 create_final_video(zus_video_name, zus_audio_name)
 print("done")
