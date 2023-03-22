6ca489ff10f445735f9287f3ba28ee0e85e34d80
Nour125 <ahmed.noureddin.00@gmail.com>
2023-03-22 21:14:18 +0100

    srt

--- test.py
+++ test.py
@@ -36,7 +36,7 @@ final_video_name = date.strftime("%d.%m.%Y") + ".final" + mp4
 
 input_file = "C:\\Users\\nourm\\OneDrive\\Desktop\\Nour\\Bot\\" + zus_audio_name
 output_file = r"C:\Users\nourm\OneDrive\Desktop\Nour\Bot" +zus_srt_name
-email = "rbot629@gmail.com"
+email = "bopaxi6270@kaudat.com"
 
 def find_By_ID(webdriver, data):
     element = webdriver.find_elements(By.ID , data)
@@ -55,40 +55,43 @@ def find_By_XPATH(webdriver, data):
 
 def get_srt():
     s = Service("chromedriver.exe")
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {r"C:\Users\nourm" : r"C:\Users\nourm\OneDrive\Desktop\Nour\Bot\VideosBot"}
+    chromeOptions.add_experimental_option("prefs",prefs)
+
     webDriver = webdriver.Chrome( service = s )
+    
+
     webDriver.get("https://www.subtitlevideo.com/")
     webDriver.implicitly_wait(10)
     
-    WebDriverWait(webDriver, 5).until(webDriver.find_element(By.ID , "language_in"))
+    #WebDriverWait(webDriver, 5).until(webDriver.find_element(By.ID , "language_in"))
+
     language_Butten = webDriver.find_element(By.ID , "language_in")
     language_Butten.send_keys("en-US")
 
-    WebDriverWait(webDriver, 5).until(find_By_ID(webDriver,"fileToUpload"))
+    #WebDriverWait(webDriver, 5).until(find_By_ID(webDriver,"fileToUpload"))
     upload_Butten = webDriver.find_element(By.ID , "fileToUpload")
     upload_Butten.send_keys(input_file)
     
-    WebDriverWait(webDriver, 5).until(find_By_XPATH(webDriver,"/html/body/div[2]/div/form/fieldset/div[4]"))
+    #WebDriverWait(webDriver, 5).until(find_By_XPATH(webDriver,"/html/body/div[2]/div/form/fieldset/div[4]"))
     row_progress_Butten = webDriver.find_element(By.XPATH , "/html/body/div[2]/div/form/fieldset/div[4]")
     while(int(row_progress_Butten.text.replace("%",""))<100):
-        time.sleep(1)
+        time.sleep(10)
     
-    WebDriverWait(webDriver, 5).until(find_By_ID(webDriver,"email"))
+    #WebDriverWait(webDriver, 5).until(find_By_ID(webDriver,"email"))
     email_Butten = webDriver.find_element(By.ID , "email")
-    email_Butten.send_keys(email)
+    email_Butten.send_keys("bopaxi6270@kaudat.com")
     
-    WebDriverWait(webDriver, 5).until(find_By_ID(webDriver,"convert_video"))
+    #WebDriverWait(webDriver, 5).until(find_By_ID(webDriver,"convert_video"))
     convert_video_Butten = webDriver.find_element(By.ID , "convert_video")
     convert_video_Butten.click()
-    
-    WebDriverWait(webDriver, 5).until(find_By_XPATH(webDriver,"/html/body/div[2]/div/div[4]/div[3]/table/tr[2]/td[5]"))
-    convert_video_done_Butten = webDriver.find_element(By.XPATH , "/html/body/div[2]/div/div[4]/div[3]/table/tr[2]/td[5]")
-    print(convert_video_done_Butten.text)
-    #time.sleep(20)
-    while(str(convert_video_done_Butten.text) != "done"):
-        WebDriverWait(webDriver, 5).until(find_By_XPATH(webDriver,"/html/body/div[2]/div/div[4]/div[3]/table/tr[2]/td[5]"))
-        print("this is the sit u want to print:"+ convert_video_done_Butten.text)
-
-    time.sleep(5)    
+    time.sleep(120)
+    #WebDriverWait(webDriver, 5).until(find_By_XPATH(webDriver,"/html/body/div[2]/div/div[4]/div[3]/table/tr[2]/td[5]"))
+    convert_video_done_Butten = webDriver.find_element(By.CLASS_NAME , "fa fa-cloud-download")
+    convert_video_done_Butten.click()
+    time.sleep(120)
+   
     
 
 
